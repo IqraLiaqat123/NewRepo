63e1234ef52f01cfd391f6e7e743a03f88c5d924
IqraLiaqat123 <43707727+IqraLiaqat123@users.noreply.github.com>
2018-10-08 01:25:08 -0700

    commit all

---
+++ PhoneBook/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneBook.Models;
+
+namespace PhoneBook.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Register(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", person);
+            }
+            else
+            {
+                //there is validation error
+                return View();
+            }
+        }
+    }
+}
---
+++ PhoneBook/Models/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBook.Models
+{
+    public class Person
+    {
+
+        public string PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string AddedOn { get; set; }
+        public string AddedBy { get; set; }
+        public string HomeAddress { get; set; }
+        public string HomeCity { get; set; }
+        public string FaceBookAccountId { get; set; }
+        public string LinkedId { get; set; }
+        public string UpdateOn { get; set; }
+        public string ImagePath { get; set; }
+        public string TwitterId { get; set; }
+        public string EmailId { get; set; }
+
+    }
+
+    public class Contact
+    {
+        public string ContactId { get; set; }
+        public string ContactNumber { get; set; }
+        public string Type { get; set; }
+        public string PersonId { get; set; }
+    }
+}
